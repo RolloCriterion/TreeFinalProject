af565be76ecb6944aab48332ff16ebb9d35c20cf
Aldo <aldo.pallucci@gmail.com>
2020-12-04 15:58:44 +0100

    Modifica EndPoitns v2

--- src/main/java/com/finalproject/EndPoints.java
+++ src/main/java/com/finalproject/EndPoints.java
@@ -24,44 +24,44 @@ public class EndPoints {
         return "Registra un utente alla piattaforma";
     }
 
-    @GetMapping("/login?username={username}&password={password}")
-    public String logIn(@PathVariable("username") String username, @PathVariable("password") String password){
+    @GetMapping("/login")
+    public String logIn(@RequestParam(name="username") String username, @RequestParam(name="password") String password){
         return "Verifica la corrispondenza di email e password e restituisce l'utente corrispondente";
     }
 
     @GetMapping("/events")
-    public List<EventService> getActiveEvents(@RequestBody EventEntity eventEntity){
+    public List<EventService> getActiveEvents(@CookieValue("username") String usernname){
         //Restituisce gli eventi disponibili a cui l'utente può registrarsi
         return null;
     }
 
     @PostMapping("/join/{eventid}")
-    public String joinEvent(@PathVariable("eventid") String eventId){
+    public String joinEvent(@PathVariable("eventid") String eventId, @CookieValue("username") String usernname){
         return "Registra l'utente ad un evento";
     }
 
     @PostMapping("/unjoin/{eventid}")
-    public String unjoinEvent(@PathVariable("eventid") String eventId){
+    public String unjoinEvent(@PathVariable("eventid") String eventId, @CookieValue("username") String usernname){
         return "Annulla la registrazione dell'utente ad un evento";
     }
 
     @PostMapping("/event")
-    public String createEvent(@RequestBody EventEntity eventEntity){
+    public String createEvent(@RequestBody EventEntity eventEntity, @CookieValue("username") String usernname){
         return "Crea un evento sulla piattaforma";
     }
 
     @GetMapping("/event/{eventid}")
-    public String getEventDetails(@PathVariable("eventid") String eventId){
+    public String getEventDetails(@PathVariable("eventid") String eventId, @CookieValue("username") String usernname){
         return "Restituisce le informazioni dettagliate di un evento.";
     }
 
     @DeleteMapping("/event/{eventid}")
-    public String cancelEvent(@PathVariable("eventid") String eventId){
+    public String cancelEvent(@PathVariable("eventid") String eventId, @CookieValue("username") String usernname){
         return "Permette solo al creatore di un evento di annullarlo.";
     }
 
     @GetMapping("/user/events")
-    public List<EventService> getUserEvents(@RequestBody EventEntity eventEntity){
+    public List<EventService> getUserEvents(@RequestBody EventEntity eventEntity, @CookieValue("username") String usernname){
         //Restituisce una lista con gli eventi creati dall'utente e quelli a cui ha partecipato. Solo gli eventi futuri, non quelli passati
         return null;
     }
