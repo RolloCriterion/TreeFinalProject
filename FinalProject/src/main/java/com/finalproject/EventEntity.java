ee3f698e8cced07185cfe8ab69a6d209465dc31c
Marco <marco.maniaci4@gmail.com>
2020-12-04 11:38:35 +0100

    Auto stash before merge of "master" and "origin/master"

--- FinalProject/src/main/java/com/finalproject/EventEntity.java
+++ FinalProject/src/main/java/com/finalproject/EventEntity.java
@@ -1,4 +1,19 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 package com.finalproject;
 
 public class EventEntity {
 }
+=======
+=======
+>>>>>>> Stashed changes
+package com.finalproject;
+
+public class EventEntity {
+
+    
+}
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
